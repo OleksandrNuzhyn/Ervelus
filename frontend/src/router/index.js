1b470bfa65fa9013b1cd04019d696f87f97c10a9
Nikita Dakhno <nikita.dakhno@student.tuke.sk>
2025-07-06 15:48:34 +0200

    06.07

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,9 +3,11 @@ import { useAuthStore } from '@/stores/auth';
 
 const routes = [
   { path: '/login', name: 'login', component: () => import('@/views/LoginPage.vue') },
-  //{ path: '/register', name: 'register', component: () => import('@/views/RegisterPage.vue') },
+  { path: '/register', name: 'register', component: () => import('@/views/RegisterPage.vue') },
   //{ path: '/dashboard', name: 'dashboard', component: () => import('@/views/DashboardPage.vue'), meta: { requiresAuth: true } },
   //{ path: '/', name: 'home', component: () => import('@/views/HomePage.vue') }
+  { path: '/forgot-password', name: 'forgot-password', component: () => import('@/components/AuthComponents/ForgotPasswordForm.vue') },
+  { path: '/reset-password-confirm/:uid/:token', name: 'reset-password-confirm', component: () => import('@/components/AuthComponents/ResetPasswordConfirm.vue') },
 ];
 
 const router = createRouter({
