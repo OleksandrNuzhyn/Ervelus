81631edfa1c589ca9cbfa98e328679a882c31ff9
Sanya <navnuzhyn@gmail.com>
2025-07-07 12:43:10 +0300

    Fix Reset Password View

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.shortcuts import redirect
 from allauth.account.views import ConfirmEmailView
-from dj_rest_auth.views import PasswordResetConfirmView
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
 
@@ -20,16 +19,5 @@ class ConfirmEmailRedirectView(ConfirmEmailView):
         redirect_url = getattr(
             settings,
             "ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL",
-        )
-        return redirect(redirect_url)
-
-
-class PasswordResetRedirectView(PasswordResetConfirmView):
-    def post(self, request, *args, **kwargs):
-        super().post(request, *args, **kwargs)
-
-        redirect_url = getattr(
-            settings,
-            "PASSWORD_RESET_REDIRECT_URL",
         )
         return redirect(redirect_url)
