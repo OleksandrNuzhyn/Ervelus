5449dab216ef1e175f13bfcbc630d2bb5f283a25
Sanya <navnuzhyn@gmail.com>
2025-07-08 12:34:21 +0300

    Password Reset Repairing

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from dj_rest_auth.views import PasswordResetConfirmView
 from users.views import ConfirmEmailRedirectView, GoogleLogin
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
+    path('api/auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('api/auth/google/', GoogleLogin.as_view(), name='google_login'),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/account-confirm-email/<str:key>/', ConfirmEmailRedirectView.as_view(), name='account_confirm_email'),
