c988746743e4202bef409a9079c6e127455adac0
FiftySanya <164347366+OleksandrNuzhyn@users.noreply.github.com>
2025-07-09 22:12:02 +0300

    Revert "Feature/registration (#7)"
    This reverts commit 87b64bd94ffb0f1a448ada568ef2962d7eefcce8.

--- frontend/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div id="app" class="min-h-screen w-full bg-gray-900 text-white">
-    <RouterView />
-  </div>
-</template>
-
-  <script setup>
-  import { RouterView } from 'vue-router'
-  </script>
