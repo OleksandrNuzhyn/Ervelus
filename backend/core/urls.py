ca2eb37087e5d4528bfac0f9d9b783fb095cd445
Sanya <navnuzhyn@gmail.com>
2025-07-10 18:14:33 +0300

    Change confirm email setup

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from dj_rest_auth.views import PasswordResetConfirmView
-from users.views import ConfirmEmailRedirectView, GoogleLogin
+from users.views import GoogleLogin
 
 
 urlpatterns = [
@@ -10,6 +10,5 @@ urlpatterns = [
     path('api/auth/password/reset/confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('api/auth/google/', GoogleLogin.as_view(), name='google_login'),
     path('api/auth/', include('dj_rest_auth.urls')),
-    path('api/auth/registration/account-confirm-email/<str:key>/', ConfirmEmailRedirectView.as_view(), name='account_confirm_email'),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
