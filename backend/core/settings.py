82b6c884fc427d6ad54b7d9a17029274ca315501
Sanya <navnuzhyn@gmail.com>
2025-07-20 18:18:10 +0300

    new db

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -166,7 +166,7 @@ DATABASES = {
         'NAME': 'postgres-ervelus-test',
         'USER': 'ervelus-test',
         'PASSWORD': 'Trueelse23',
-        'HOST': '34.118.30.161',
+        'HOST': '34.118.74.91',
         'PORT': '5432',
     },
     'async': {
@@ -174,7 +174,7 @@ DATABASES = {
         'NAME': 'postgres-ervelus-test',
         'USER': 'ervelus-test',
         'PASSWORD': 'Trueelse23',
-        'HOST': '34.118.30.161',
+        'HOST': '34.118.74.91',
         'PORT': '5432',
     }
 }
