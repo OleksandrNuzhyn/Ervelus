457f8c170e7ff7d941996a714a49397a80744b1c
Sanya <navnuzhyn@gmail.com>
2025-08-17 19:53:24 +0300

    statistic added

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Style, SubscriptionPlan, Genre
 
 
@@ -9,8 +10,16 @@ class SubscriptionPlanAdmin(admin.ModelAdmin):
 
 @admin.register(Style)
 class StyleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'genre')
+    list_display = ('id', 'name', 'genre', 'generation_count')
     list_select_related = ('genre',)
 
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(generation_count=Count("generation_requests")).order_by("-generation_count")
+        return queryset
+
+    def generation_count(self, obj):
+        return obj.generation_count
+    
 
 admin.site.register(Genre)
