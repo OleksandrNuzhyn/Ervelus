0564fdec9e9dfaf14455961a83de3ca19d8f08b2
Sanya <navnuzhyn@gmail.com>
2025-08-22 14:57:19 +0300

    config endpoint

---
+++ backend/core/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import ApplicationConfig
+
+
+class ApplicationConfigSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApplicationConfig
+        fields = ['is_registration_enabled']
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from debug_toolbar.toolbar import debug_toolbar_urls
+from . import views
 
 
 urlpatterns = []
@@ -9,12 +10,13 @@ urlpatterns = []
 if settings.DEBUG:
     urlpatterns += [
         path('admin/', admin.site.urls),
-
+        
         path('webhooks/subscriptions/', include('subscriptions.urls_paddle_webhooks')),
         path('webhooks/subscriptions/', include('subscriptions.urls_tasks_webhooks')),
         path('webhooks/generations/', include('generations.urls_webhooks')),
 
         path('api/auth/', include('users.urls')),
+        path('api/core/app-config/', views.app_config_details, name='app-config-details'),
         path('api/subscriptions/', include('subscriptions.urls_api')),
         path('api/products/', include('products.urls')),
         path('api/generations/', include('generations.urls_api')),
@@ -27,6 +29,7 @@ else:
             path('webhooks/subscriptions/', include('subscriptions.urls_paddle_webhooks')),
 
             path('api/auth/', include('users.urls')),
+            path('api/core/app-config/', views.app_config_details, name='app-config-details'),
             path('api/subscriptions/', include('subscriptions.urls_api')),
             path('api/products/', include('products.urls')),
             path('api/generations/', include('generations.urls_api'))
