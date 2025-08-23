8e1af38e517fced3e67ae99f81d17e73684c944a
Sanya <navnuzhyn@gmail.com>
2025-08-23 17:37:21 +0300

    mailgun list syncing

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import UserProfile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth import get_user_model
+from django.urls import path
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from . import services
+
+User = get_user_model()
+admin.site.unregister(User)
 
 
 @admin.register(UserProfile)
@@ -14,3 +23,26 @@ class UserProfileAdmin(admin.ModelAdmin):
     @admin.display(ordering='total_credits', description='total credits')
     def total_credits(self, obj):
         return obj.total_credits
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    change_list_template = "admin/users/user/change_list.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('sync-mailgun-list/', self.admin_site.admin_view(self.sync_mailgun_list_view), name='sync_mailgun_list_view')
+        ]
+
+        return custom_urls + urls
+
+    def sync_mailgun_list_view(self, request):
+        result = services.sync_users_with_mailgun_list()
+
+        if result["is_success"]:
+            self.message_user(request, result["message"], messages.SUCCESS)
+        else:
+            self.message_user(request, result["message"], messages.ERROR)
+        
+        return HttpResponseRedirect("../")
