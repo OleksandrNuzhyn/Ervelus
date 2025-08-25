f1eea4d7a608c2d3cc0a31f027d4cc375a62abfa
Sanya <navnuzhyn@gmail.com>
2025-08-25 23:17:18 +0300

    fix

--- backend/core/views.py
+++ backend/core/views.py
@@ -7,7 +7,7 @@ from .serializers import ApplicationConfigSerializer
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def app_config_details(request):
-    config = ApplicationConfig.objects.get_solo()
+    config = ApplicationConfig.get_solo()
     serializer = ApplicationConfigSerializer(config)
 
     return Response(serializer.data, status=200)
