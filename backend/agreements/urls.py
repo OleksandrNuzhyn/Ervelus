870810e24ada6141c318418b683c81309caaf16d
Sanya <navnuzhyn@gmail.com>
2025-08-25 17:16:30 +0300

    pass fix

--- backend/agreements/urls.py
+++ backend/agreements/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('published/', views.published_agreements_list_view, name='published-list'),
     path('pending/', views.pending_agreements_list_view, name='pending-list'),
-    path('accept/', views.accept_agreements_view, name='accept'),
+    path('accept/', views.accept_documents_latest_version, name='accept'),
 ]
