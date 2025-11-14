00578e8185e9fbb3e4db6832798a43813016c242
sanya <navnuzhyn@gmail.com>
2025-11-14 22:48:32 +0200

    auth fix

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -13,7 +13,7 @@ from .forms import EmailForm
 from django.conf import settings
 from django_otp.plugins.otp_totp.models import TOTPDevice
 from django_otp.plugins.otp_totp.admin import TOTPDeviceAdmin
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import TokenProxy
 from core.admin_mixins import NoLogAdminMixin
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.models import SocialAccount
@@ -30,7 +30,7 @@ admin.site.unregister(SocialAccount)
 admin.site.unregister(SocialApp)
 admin.site.unregister(SocialToken)
 admin.site.unregister(TOTPDevice)
-admin.site.unregister(Token)
+admin.site.unregister(TokenProxy)
 
 EmailAddress._meta.verbose_name = "Email Address"
 EmailAddress._meta.verbose_name_plural = "Email Addresses"
