9644c351e5c716a51def4e6098795496be94fa97
sanya <navnuzhyn@gmail.com>
2025-12-02 15:03:26 +0200

    fixed pricing

--- backend/subscriptions/views.py
+++ backend/subscriptions/views.py
@@ -13,7 +13,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-@api_view(['GET'])
+@api_view(['POST'])
 @permission_classes([AllowAny])
 def subscription_eligibility_check(request):
     serializer = SubscriptionEligibilityCheckSerializer(data=request.data)
