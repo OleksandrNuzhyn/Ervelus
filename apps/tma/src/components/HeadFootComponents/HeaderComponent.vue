163780e5c57e942250744ac414e9905ef228b0e9
sanya <navnuzhyn@gmail.com>
2026-01-19 14:45:39 +0200

    tma ui fix

--- apps/tma/src/components/HeadFootComponents/HeaderComponent.vue
+++ apps/tma/src/components/HeadFootComponents/HeaderComponent.vue
@@ -34,7 +34,7 @@
             </div>
             <span>{{ $t('navigation.coins') }}: {{ credits }}</span>
           </div>
-          <router-link to="/dashboard" class="hover:text-gray-50 flex items-center gap-2">
+          <router-link to="/" class="hover:text-gray-50 flex items-center gap-2">
             <div class="w-10 flex justify-center">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-9 w-9" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
